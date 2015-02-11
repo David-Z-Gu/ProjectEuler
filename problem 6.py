168ccc9fbcf5ff314fa82037f7b70a21667b1219
David Zheyuan Gu <neo.zheyuan@gmail.com>
2015-02-10 20:54:02 -0600

    Create problem 6.py

---
+++ problem 6.py
@@ -0,0 +1,14 @@
+k=0
+j = 0
+sum = 0
+for i in range (1,101):
+    j = (k + i)*(k + i)
+    sum = sum + j
+
+k=0
+j = 0
+sum = 0
+for i in range (1,101):
+    j = (k + i)
+    sum = (sum + j)
+    I square the sum and subtract the result I got from the first half.
