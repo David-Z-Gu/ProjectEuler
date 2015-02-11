e0b3ef67823b4b579f9a352909ae7477b0c9e62f
David Zheyuan Gu <neo.zheyuan@gmail.com>
2015-02-10 20:53:30 -0600

    Create problem 2.py

---
+++ problem 2.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+c = 0
+sum = 0
+while c < 4000000:
+    c = a + b
+    a = b
+    b = c
+    if c%2 ==0:
+        sum = sum + c
+    else:
+     c = c
+result + 2 is the correct answer
