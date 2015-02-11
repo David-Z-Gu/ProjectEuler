f480220643f87024657a0ff884069fc08947cd6d
David Zheyuan Gu <neo.zheyuan@gmail.com>
2015-02-10 20:52:54 -0600

    Create Problem 1.py

---
+++ Problem 1.py
@@ -0,0 +1,24 @@
+three = 3
+five = 5
+a = 0
+i = 1
+total = 0
+while a < 1000:
+    i = i + 1
+    a = three
+    total = total + a
+three = 3 * i
+
+three = 3
+five = 5
+fifteen = 15
+a = 0
+i = 1
+total = 0
+while a < 1000:
+    i = i + 1
+    a = fifteen
+    total = total + a
+    fifteen = 15 * i
+
+I then minus 1002 from the result
